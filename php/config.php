86a581807aa79266af75a7845f5f2f3b1b0ef21d
Sachita <sachita2825@gmail.com>
2026-05-12 13:02:07 +0530

    Updated Railway database

--- php/config.php
+++ php/config.php
@@ -1,6 +1,12 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "chatbot_db");
+$conn = new mysqli(
+    "yamabiko.proxy.rlwy.net",
+    "root",
+    "fdhKAdvYWlRpwNfQUGFpwwyNSLwZrsLK",
+    "railway",
+    17266
+);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
